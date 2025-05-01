7075fc8531d4187b4e727481cf6f597412ffd1f5
Akash <hafejmohammadakash@gmail.com>
2025-05-01 23:56:50 +0600

    state ke useeffect er moddhe dore rakham

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import { signInWithPopup, TwitterAuthProvider } from 'firebase/auth';
+import { onAuthStateChanged, signInWithPopup, TwitterAuthProvider } from 'firebase/auth';
 import { GoogleAuthProvider, GithubAuthProvider} from 'firebase/auth';
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { auth } from '../../firebase.init';
 export const AuthContext = createContext(null)
 
@@ -10,12 +10,13 @@ const AuthProvider = ({children}) => {
     const googleProvider = new GoogleAuthProvider()
     const githubProvider = new GithubAuthProvider()
     const TwitterProvider = new TwitterAuthProvider()
+    const[users, setUsers] = useState(null)
 
     //google login
     const handleGoogleLogin = () => {
         signInWithPopup(auth, googleProvider)
         .then((result) => {
-            console.log(result.user)
+            setUsers(result.user)
         })
     }
 
@@ -23,7 +24,7 @@ const AuthProvider = ({children}) => {
     const handleGithubLogin = () => {
         signInWithPopup(auth, githubProvider)
         .then((result) => {
-            console.log(result.user)
+            setUsers(result.user)
         })
     }
 
@@ -31,12 +32,21 @@ const AuthProvider = ({children}) => {
     const handleTwitterLogin = () => {
         signInWithPopup(auth, TwitterProvider)
         .then((result) => {
-            console.log(result.user)
+            setUsers(result.user)
         })
     }
 
 
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log(currentUser)
+            
+        })
 
+        return () => {
+            unSubscribe()
+        }
+    }, [])
     
 
     const authInfo = {
